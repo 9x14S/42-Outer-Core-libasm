5568526b7d115fb47b4d70a4d6da2a10d039eb15
Salah-Eddine Rahhali <srahhali@student.42barcelona.com>
2026-01-23 23:04:50 +0100

    add: ft_strcmp

---
+++ ft_strcmp.s
@@ -0,0 +1,22 @@
+BITS 64
+
+GLOBAL ft_strcmp
+
+ft_strcmp:
+.ft_strcmp_loop:
+	cmp [rdi], byte 0x00
+	je .ft_strcmp_end
+	cmp [rsi], byte 0x00
+	je .ft_strcmp_end
+	mov cl, byte[rsi]
+	cmp [rdi], cl
+	jne .ft_strcmp_end
+
+	inc rdi
+	inc rsi
+	jmp .ft_strcmp_loop
+.ft_strcmp_end:
+	movzx eax, byte [rdi]
+	movzx ecx, byte [rsi]
+	sub eax, ecx
+	ret
