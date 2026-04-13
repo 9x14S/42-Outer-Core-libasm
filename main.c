e7be555344b0c26bedf86f770333415abfbfe81b
9x14S <Contact9x14S@proton.me>
2026-04-13 03:45:59 +0200

    update: tests for errno-modifying functions (write, read), add: colors

--- main.c
+++ main.c
@@ -5,18 +5,36 @@
 
 #include "libasm.h"
 
+#define GREEN "\e[0;32m"
+#define RED "\e[0;31m"
+#define RESET "\e[0m"
+
+#define PASS GREEN "PASS" RESET
+#define FAIL RED "FAIL" RESET
+
 #define TEST(x, y) do {\
-long x_val = (long)x;\
-long y_val = (long)y;\
-printf("TESTING: " #x " and " #y "\nRESULT: (%ld), (%ld)\nGRADE:%s\n",\
-x_val, y_val, x_val == y_val ? "PASS" : "FAIL");\
+	long x_val = (long)x;\
+	long y_val = (long)y;\
+	printf("TESTING: " #x " and " #y "\nRESULT: (%ld), (%ld)\nGRADE:%s\n",\
+		x_val, y_val, x_val == y_val ? PASS : FAIL );\
 } while (0)
 
 #define TEST_CMP(x, y) do {\
-long x_val = (long)x;\
-long y_val = (long)y;\
-printf("TESTING: " #x " and " #y "\nGRADE:%s\n",\
-strcmp((char *)x_val, (char *)y_val) == 0 ? "PASS" : "FAIL");\
+	long x_val = (long)x;\
+	long y_val = (long)y;\
+	printf("TESTING: " #x " and " #y "\nGRADE:%s\n",\
+		strcmp((char *)x_val, (char *)y_val) == 0 ? PASS : FAIL);\
+} while (0)
+
+#define TEST_ERRNO(x, y) do {\
+	errno = 0;						\
+	ssize_t x_val = (ssize_t)x;		\
+	int		x_errno = errno;		\
+	ssize_t y_val = (ssize_t)y;		\
+	int		y_errno = errno;		\
+	printf("TESTING: " #x " and " #y "\nRESULT: (%ld), (%ld)\nERRNO: (%d), (%d)\n"	\
+			"GRADE:%s\n", x_val, y_val, x_errno, y_errno,							\
+			x_errno == y_errno && x_val == y_val ? PASS : FAIL );					\
 } while (0)
 
 int main(void)
@@ -43,59 +61,23 @@ int main(void)
 	TEST_CMP(ft_strcpy(test1, ""), strcpy(test2, ""));
 
 	// strdup tests
+	TEST_CMP(ft_strdup(""), strdup(""));
 	TEST_CMP(ft_strdup("aaaaaaaaaaaaa"), strdup("aaaaaaaaaaaaa"));
 
-	errno = 0;
-	ft_write(1, NULL, 0xffff);
-	printf("ft_write errno = %d\n", errno);
-	errno = 0;
-	write(1, NULL, 0xffff);
-	printf("write errno    = %d\n", errno);
+	// write tests
+	#define TEST_STR "TEST42\n"
+	TEST_ERRNO(ft_write(0, NULL, 0), write(0, NULL, 0));
+	TEST_ERRNO(ft_write(1, NULL, 0xffff), write(1, NULL, 0xffff));
+	TEST_ERRNO(ft_write(-1, &main, 0xffff), write(-1, &main, 0xffff));
+	TEST_ERRNO(ft_write(-1, NULL, 0xffff), write(-1, NULL, 0xffff));
+	TEST_ERRNO(ft_write(1, TEST_STR, sizeof(TEST_STR)), write(1, TEST_STR, sizeof(TEST_STR)));
+	TEST_ERRNO(ft_write(1, "", 0), write(1, "", 0));
+	#undef TEST_STR
 
-	errno = 0;
-	ft_write(-1, &main, 0xffff);
-	printf("ft_write errno = %d\n", errno);
-	errno = 0;
-	write(-1, &main, 0xffff);
-	printf("write errno    = %d\n", errno);
-
-	errno = 0;
-	ft_write(-1, NULL, 0xffff);
-	printf("ft_write errno = %d\n", errno);
-	errno = 0;
-	write(-1, NULL, 0xffff);
-	printf("write errno    = %d\n", errno);
-
-	#define WHATEVER "TITS\n"
-	errno = 0;
-	ft_write(1, WHATEVER, sizeof(WHATEVER));
-	printf("ft_write errno = %d\n", errno);
-	errno = 0;
-	write(1, WHATEVER, sizeof(WHATEVER));
-	printf("write errno    = %d\n", errno);
-
-
-	errno = 0;
-	ft_read(1, NULL, 0xffff);
-	printf("ft_read errno = %d\n", errno);
-	errno = 0;
-	read(1, NULL, 0xffff);
-	printf("read errno    = %d\n", errno);
-
-	errno = 0;
-	ft_read(-1, &main, 0xffff);
-	printf("ft_read errno = %d\n", errno);
-	errno = 0;
-	read(-1, &main, 0xffff);
-	printf("read errno    = %d\n", errno);
-
-	errno = 0;
-	ft_read(-1, NULL, 0xffff);
-	printf("ft_read errno = %d\n", errno);
-	errno = 0;
-	read(-1, NULL, 0xffff);
-	printf("read errno    = %d\n", errno);
-
+	// read tests (shouldn't crash but also shouldn't do anything)
+	TEST_ERRNO(ft_read(0, NULL, 0), read(0, NULL, 0));
+	TEST_ERRNO(ft_read(-1, &main, 0xffff), read(-1, &main, 0xffff));
+	TEST_ERRNO(ft_read(-1, NULL, 0xffff), read(-1, NULL, 0xffff));
 
 	return (0);
 }
