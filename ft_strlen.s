bbd583d2500f9f9e34e8c3de334eb8e794d6f5b1
9x14S <Contact9x14S@proton.me>
2026-04-13 03:46:55 +0200

    fix: cmp on word width instead of bytes

--- ft_strlen.s
+++ ft_strlen.s
@@ -7,10 +7,9 @@ SECTION .text
 ft_strlen:
 	xor rax, rax
 .ft_strlen_loop:
-	cmp byte [rdi], byte 0x00
+	cmp byte [rdi+rax], byte 0x00
 	jz .ft_strlen_end
 	inc rax
-	inc rdi
 	jmp .ft_strlen_loop
 .ft_strlen_end:
 	ret
