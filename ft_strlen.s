e677dd15018820fb24cfe28a73561ae820c079a0
9x14S <Contact9x14S@proton.me>
2026-04-13 03:07:20 +0200

    fix: wrong jump used, je -> jz. Caused overread

--- ft_strlen.s
+++ ft_strlen.s
@@ -2,11 +2,13 @@ BITS 64
 
 GLOBAL ft_strlen
 
+SECTION .text
+
 ft_strlen:
 	xor rax, rax
 .ft_strlen_loop:
-	cmp [rdi], byte 0x00
-	je .ft_strlen_end
+	cmp byte [rdi], byte 0x00
+	jz .ft_strlen_end
 	inc rax
 	inc rdi
 	jmp .ft_strlen_loop
